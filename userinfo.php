830ff401acf3e33d0f47e2680375faf6743a9e28
Manish Singh <39923325+ManishSingh2710@users.noreply.github.com>
2020-10-20 16:42:52 +0530

    Tech

---
+++ userinfo.php
@@ -0,0 +1,29 @@
+<?php
+
+$con = mysqli_connect('localhost','root');
+
+if($con)
+{
+	echo "Connection successful";
+}else
+{
+	echo "No connection";
+}
+
+mysqli_select_db($con, 'gec2020');
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$clg = $_POST['clg'];
+$psw = $_POST['psw'];
+//$id = $_POST['id'];
+//var_dump($email);
+$query = "insert into userinfo (name, email, phone, clg, psw)
+		  values('$name', '$email', '$phone', '$clg', '$psw') ";
+
+mysqli_query($con,$query);
+
+header('location:index.html?s=1');
+
+?>
